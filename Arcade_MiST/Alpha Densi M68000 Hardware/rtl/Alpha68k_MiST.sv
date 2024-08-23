ff020da80dd8b524216249d305be1162e5492163
tdelage26 <tdelage@spiderweb-france.com>
2024-08-23 22:22:58 +0200

    Update MRAs

--- Arcade_MiST/Alpha Densi M68000 Hardware/rtl/Alpha68k_MiST.sv
+++ Arcade_MiST/Alpha Densi M68000 Hardware/rtl/Alpha68k_MiST.sv
@@ -536,7 +536,7 @@ wire m_up4, m_down4, m_left4, m_right4, m_up4B, m_down4B, m_left4B, m_right4B;
 wire m_tilt, m_coin1, m_coin2, m_coin3, m_coin4, m_one_player, m_two_players, m_three_players, m_four_players;
 wire [11:0] m_fire1, m_fire2, m_fire3, m_fire4;
 
-arcade_inputs inputs (
+arcade_inputs #(.START1(10), .START2(12), .COIN1(11)) inputs (
 	.clk         ( clk_72      ),
 	.key_strobe  ( key_strobe  ),
 	.key_pressed ( key_pressed ),
