200b1bbcee25ce1a7acf0f7912c1b53a625dcaf5
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2024-09-19 21:08:09 +0200

    V33: make it work with old Quartus

--- common/CPU/v33/types.sv
+++ common/CPU/v33/types.sv
@@ -1,22 +1,22 @@
 package types;
     `include "enums.svh"
     
-    const bit [15:0] STACK_AW         = 16'h0001;
-    const bit [15:0] STACK_CW         = 16'h0002;
-    const bit [15:0] STACK_DW         = 16'h0004;
-    const bit [15:0] STACK_BW         = 16'h0008;
-    const bit [15:0] STACK_SP         = 16'h0010;
-    const bit [15:0] STACK_SKIP_SP    = 16'h0020;
-    const bit [15:0] STACK_BP         = 16'h0040;
-    const bit [15:0] STACK_IX         = 16'h0080;
-    const bit [15:0] STACK_IY         = 16'h0100;
-    const bit [15:0] STACK_DS1        = 16'h0200;
-    const bit [15:0] STACK_PSW        = 16'h0400;
-    const bit [15:0] STACK_PS         = 16'h0800;
-    const bit [15:0] STACK_SS         = 16'h1000;
-    const bit [15:0] STACK_DS0        = 16'h2000;
-    const bit [15:0] STACK_PC         = 16'h4000;
-    const bit [15:0] STACK_OPERAND    = 16'h8000;
+    localparam bit [15:0] STACK_AW         = 16'h0001;
+    localparam bit [15:0] STACK_CW         = 16'h0002;
+    localparam bit [15:0] STACK_DW         = 16'h0004;
+    localparam bit [15:0] STACK_BW         = 16'h0008;
+    localparam bit [15:0] STACK_SP         = 16'h0010;
+    localparam bit [15:0] STACK_SKIP_SP    = 16'h0020;
+    localparam bit [15:0] STACK_BP         = 16'h0040;
+    localparam bit [15:0] STACK_IX         = 16'h0080;
+    localparam bit [15:0] STACK_IY         = 16'h0100;
+    localparam bit [15:0] STACK_DS1        = 16'h0200;
+    localparam bit [15:0] STACK_PSW        = 16'h0400;
+    localparam bit [15:0] STACK_PS         = 16'h0800;
+    localparam bit [15:0] STACK_SS         = 16'h1000;
+    localparam bit [15:0] STACK_DS0        = 16'h2000;
+    localparam bit [15:0] STACK_PC         = 16'h4000;
+    localparam bit [15:0] STACK_OPERAND    = 16'h8000;
     
     typedef struct {
         bit [15:0] pc;
